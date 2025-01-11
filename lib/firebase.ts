08af4f0264105b2490f07cc84001c9430afe62a2
DarrellS <152986445+Grimarks@users.noreply.github.com>
2025-01-12 05:36:02 +0700

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,7 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
