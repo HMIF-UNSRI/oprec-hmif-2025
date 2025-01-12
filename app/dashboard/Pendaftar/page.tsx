341de52e64254a040848dda81585e3e5369d8316
DarrellS <152986445+Grimarks@users.noreply.github.com>
2025-01-13 00:44:12 +0700

    Add files via upload

---
+++ app/dashboard/Pendaftar/page.tsx
@@ -0,0 +1,14 @@
+import TableCalonStaff from "@/components/molecules/TableCalonStaff";
+
+export default function Pendaftar() {
+    return (
+        <main className="flex items-center justify-center px-2 lg:px-10">
+            <div className="flex flex-col justify-center px-5 py-10 mx-auto rounded-lg table-calon-staff">
+                <h1 className="text-xl font-semibold lg:text-2xl text-slate-200">
+                    Calon Staff
+                </h1>
+                <TableCalonStaff />
+            </div>
+        </main>
+    );
+}
